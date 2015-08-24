c814b12d9f5484395b56959bac76ccc21a9f9057
Adrian Haasler García <dev@adrianhaasler.com>
2015-08-24 07:47:59 +0000

    Improve postversion script

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp');
+var shell = require('gulp-shell');
+var inquirer = require('inquirer');
 var $ = require('gulp-load-plugins')();
 var rirmaf = require('rimraf');
 
@@ -35,3 +37,28 @@ gulp.task('watch', function(){
 });
 
 gulp.task('test', ['mocha', 'jshint']);
+
+gulp.task('git-show', shell.task([
+  'git show -1'
+]));
+
+gulp.task('git-push', shell.task([
+  'git push',
+  'git push --tags'
+]));
+
+gulp.task('git-push:confirm', ['git-show'], function(done) {
+  inquirer.prompt([{
+    type: 'confirm',
+    message: 'Push version?',
+    default: false,
+    name: 'push'
+  }], function(answers) {
+    if(answers.push) {
+      gulp.start('git-push');
+    }
+    done();
+  });
+});
+
+gulp.task('release', ['git-push:confirm']);
